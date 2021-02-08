a571ce5f4e423bb3fb0d6246edd9166cc5db22c3
DeborahBarbosaOliveira <73758050+DeborahBarbosaOliveira@users.noreply.github.com>
2021-02-08 09:17:18 +0000

    AddedDeveloperInfo

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <SearchEngine />
+        <span id="git-hub"> <strong>Open-source code by </strong> <br /><a href="https://github.com/DeborahBarbosaOliveira/SheCodesForecast" id="my-github">Deborah Barbosa</a></span> 
     </div>
   );
 }
