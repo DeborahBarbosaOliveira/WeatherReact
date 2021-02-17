7ef6131ddb154c8d737f028fa0fd0346546ceb54
DeborahBarbosaOliveira <73758050+DeborahBarbosaOliveira@users.noreply.github.com>
2021-02-17 11:51:46 +0000

    AddedAPIIntegration

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import "./index.css";
-import SearchEngine from "./Weather";
+import Weather from "./Weather";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Weather App</h1>
-      <SearchEngine />
-        <footer id="git-hub"> <a href="https://github.com/DeborahBarbosaOliveira/SheCodesForecast" id="my-github" target="_blank">Open-source code</a> by <a href="https://www.linkedin.com/in/deborah-barbosa-oliveira/" id="my-linkedin" target="_blank">Deborah barbosa</a></footer> 
-    </div>
+                
+      <Weather />
+        
+ 
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,46 +1,60 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-export default function SearchEngine() {
-  let [input, setInput] = useState("");
-  let [message, setMessage] = useState("");
+export default function Weather() {
+  const [ready, setReady] = useState(false);
+  const [weatherData, setWeatherData] = useState({});
 
-  function showDetails(response) {
-    let imgURL = `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`;
-    setMessage(
-      <ul>
-        <li>Temperature: {Math.round(response.data.main.temp)}°C</li>
-        <li>Description: {response.data.weather[0].description}</li>
-        <li>Humidity: {response.data.main.humidity} %</li>
-        <li>Wind: {Math.round(response.data.wind.speed)} km/h</li>
-        <li>
-          <img src={imgURL} alt="weather Icon" />
-        </li>
-      </ul>
-    );
+  function handleResponse(response){
+
+    console.log(response.data);
+    setWeatherData({
+temperature: response.data.main.temp,
+wind: response.data.wind.speed,
+city: response.data.name,
+humidity: response.data.main.humidity,
+description: response.data.weather[0].description,
+
+    })
+    
+    setReady(true);
   }
 
-  function submitForm(event) {
-    event.preventDefault();
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=8ace475fd8f2a50f825109d1b6a3c226&units=metric`;
-    axios.get(apiUrl).then(showDetails);
-  }
-
-  function changeInput(event) {
-    setInput(event.target.value);
-  }
-
-  return (
-    <div className="SearchEngine">
-      <form onSubmit={submitForm}>
-        <input
-          type="search"
-          placeholder="Type city here"
-          onChange={changeInput}
-        />
-        <button type="Submit">Search</button>
-      </form>
-      <p>{message}</p>
+  if (ready){
+ return (
+        <div className="App">
+       <div className="container">
+    <div className="card-group">
+      <div className="card">
+      <h1 class="city" id="current-city">{weatherData.city}</h1>
+       <h2 id="date">Tue, Feb/16 | 11:01 </h2>
+        <div class="card border-danger mb-3" class="current-weather">
+                       <img
+                       src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                       class="float-left"
+                       id="icon"
+                       />
+                         <div class="card-header">
+                  <span class="current-temp" id="city-temp"> {Math.round(weatherData.temperature)}</span> <a href="#" id="celsius-link" class="active">°C</a> | <a href="#" id="fahrenheit-link">F°</a>
+                <br /> <p id ="temperature-description" className="text-capitalize"> {weatherData.description}</p></div>
+            <div class="card-body text-danger">
+              <h5 class="card-title">Real feel: 20<span id="real-feel"></span><span id="metric">°</span></h5>
+              <p class="card-text"> {weatherData.humidity}%<i class="fas fa-tint"></i> <span id="humidity"> </span> <br />
+                <i class="fas fa-wind"></i> Wind: {Math.round(weatherData.wind)} <span id="speed"></span><span id="wind-unit"> km/h</span></p>
+            </div>               
+          
     </div>
+    </div>
+    </div> 
+     </div>
+    <footer id="git-hub"> <a href="https://github.com/DeborahBarbosaOliveira/SheCodesForecast" id="my-github" target="_blank">Open-source code</a> <br /> by <a href="https://www.linkedin.com/in/deborah-barbosa-oliveira/" id="my-linkedin" target="_blank">Deborah barbosa</a></footer> 
+    </div>
+    
   );
+  } else {
+    let city = "Belo Horizonte"
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=8ace475fd8f2a50f825109d1b6a3c226&units=metric`;
+  axios.get(apiUrl).then(handleResponse); 
+
+  return "loading..."}
 }
